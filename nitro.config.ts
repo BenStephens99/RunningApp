d715d67de802cb0b0de0649c10697e24ed96615f
Ben Stephens <ben@stephens.uk.net>
2026-01-02 00:32:48 +0100

    fix

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,9 +1,10 @@
 import { defineNitroConfig } from "nitro/config";
 
 export default defineNitroConfig({
-  noExternal: ["@tabler/icons-react"],
+  externals: {
+    inline: ["@tabler/icons-react"],
+  },
   experimental: {
     wasm: true,
   },
 });
-
