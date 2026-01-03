bde49b18d79f5fe99179887ec7f33f99d0431edb
Ben Stephens <ben@stephens.uk.net>
2026-01-03 16:45:26 +0100

    refetch stale data

--- src/utils/queryClient.ts
+++ src/utils/queryClient.ts
@@ -4,7 +4,7 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 minutes
-      refetchOnWindowFocus: false,
+      refetchOnWindowFocus: true,
     },
   },
 });
